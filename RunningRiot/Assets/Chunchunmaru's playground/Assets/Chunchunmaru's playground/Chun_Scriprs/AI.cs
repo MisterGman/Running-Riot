6fe1f029e048e3d8378bc154c21a634c7a224dba
MisterGman <galem009@gmail.com>
2018-04-15 21:18:24 +0200

    Not finished
    2.5D movement and jump

---
+++ RunningRiot/Assets/Chunchunmaru's playground/Assets/Chunchunmaru's playground/Chun_Scriprs/AI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI : MonoBehaviour {
+    Player player;
+
+    void Start()
+    {
+        player = GetComponent<Player>();
+    }
+
+    void Update()
+    {
+
+        player.SetDirectionalInput(new Vector2(1,0));
+    }
+}
---
+++ RunningRiot/Assets/Chunchunmaru's playground/Assets/Chunchunmaru's playground/Chun_Scriprs/Attack_Player.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack_Player : MonoBehaviour
+{
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Enemy" && Input.GetKeyDown(KeyCode.Keypad5))
+        {
+            transform.root.SendMessage("RecieveDamageFromEnemy");
+            Debug.Log("---------------------------HIT");
+        }
+    }
+}
