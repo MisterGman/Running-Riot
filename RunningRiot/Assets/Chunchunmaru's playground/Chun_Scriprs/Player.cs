05322053f391a84b5f53600e55e2823dee5f8300
MisterGman <galem009@gmail.com>
2018-04-22 18:26:10 +0200

    Player with hook
    There is prefabs for obsticles and player to use hook.
    //Player go through walls on the left

---
+++ RunningRiot/Assets/Chunchunmaru's playground/Chun_Scriprs/Player.cs
@@ -0,0 +1,211 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Controller2D))]
+public class Player : MonoBehaviour
+{
+    protected float maxHP = 100; // the highest available level of health
+    public float currHP = 100; // current health
+
+    public float maxJumpHeight = 4;
+    public float minJumpHeight = 1;
+    public float timeToJumpApex = .4f;
+    float accelerationTimeAirborne = .2f;
+    float accelerationTimeGrounded = .1f;
+    public float moveSpeed = 20f;
+
+    public Vector2 wallJumpClimb;
+    public Vector2 wallJumpOff;
+    public Vector2 wallLeap;
+
+    public float wallSlideSpeedMax = 3;
+    public float wallStickTime = .25f;
+    float timeToWallUnstick;
+
+    public float gravity;
+    float maxJumpVelocity;
+    float minJumpVelocity;
+    Vector3 velocity;
+    float velocityXSmoothing;
+
+    private Attack_Player attack;
+    Controller2D controller;
+
+    Vector2 directionalInput;
+    bool wallSliding;
+    int wallDirX;
+
+    void Start()
+    {
+        attack = GetComponentInChildren<Attack_Player>();
+        controller = GetComponent<Controller2D>();
+
+        moveSpeed = 20f;
+
+        gravity = -(2 * maxJumpHeight) / Mathf.Pow(timeToJumpApex, 2);
+        maxJumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
+        minJumpVelocity = Mathf.Sqrt(2 * Mathf.Abs(gravity) * minJumpHeight);
+    }
+
+    void Update()
+    {
+        CalculateVelocity();
+        HandleWallSliding();
+
+        controller.Move(velocity  * Time.deltaTime, directionalInput);
+
+            
+           // this.transform.rotation.x = -this.transform.rotation.x;
+        
+
+        if (controller.collisions.above || controller.collisions.below)
+        {
+            if (controller.collisions.slidingDownMaxSlope)
+            {
+                velocity.y += controller.collisions.slopeNormal.y * -gravity * Time.deltaTime;
+            }
+            else
+            {
+                velocity.y = 0;
+            }
+        }
+    }
+
+    public void SetDirectionalInput(Vector2 input)
+    {
+        
+        directionalInput = input;
+    }
+
+    public void OnJumpInputDown()
+    {
+        if (wallSliding)
+        {
+            if (wallDirX == directionalInput.x)
+            {
+                velocity.x = -wallDirX * wallJumpClimb.x;
+                velocity.y = wallJumpClimb.y;
+            }
+            else if (directionalInput.x == 0)
+            {
+                velocity.x = -wallDirX * wallJumpOff.x;
+                velocity.y = wallJumpOff.y;
+            }
+            else
+            {
+                velocity.x = -wallDirX * wallLeap.x;
+                velocity.y = wallLeap.y;
+            }
+        }
+        if (controller.collisions.below)
+        {
+            if (controller.collisions.slidingDownMaxSlope)
+            {
+                if (directionalInput.x != -Mathf.Sign(controller.collisions.slopeNormal.x))
+                { // not jumping against max slope
+                    velocity.y = maxJumpVelocity * controller.collisions.slopeNormal.y;
+                    velocity.x = maxJumpVelocity * controller.collisions.slopeNormal.x;
+                }
+            }
+            else
+            {
+                velocity.y = maxJumpVelocity;
+            }
+        }
+    }
+
+    public void OnJumpInputUp()
+    {
+        if (velocity.y > minJumpVelocity)
+        {
+            velocity.y = minJumpVelocity;
+        }
+    }
+
+
+    void HandleWallSliding()
+    {
+        wallDirX = (controller.collisions.left) ? -1 : 1;
+        wallSliding = false;
+        if ((controller.collisions.left || controller.collisions.right) && !controller.collisions.below && velocity.y < 0)
+        {
+            wallSliding = true;
+
+            if (velocity.y < -wallSlideSpeedMax)
+            {
+                velocity.y = -wallSlideSpeedMax;
+            }
+
+            if (timeToWallUnstick > 0)
+            {
+                velocityXSmoothing = 0;
+                velocity.x = 0;
+
+                if (directionalInput.x != wallDirX && directionalInput.x != 0)
+                {
+                    timeToWallUnstick -= Time.deltaTime;
+                }
+                else
+                {
+                    timeToWallUnstick = wallStickTime;
+                }
+            }
+            else
+            {
+                timeToWallUnstick = wallStickTime;
+            }
+
+        }
+
+    }
+
+    void CalculateVelocity()
+    {
+        if (this.GetComponent<GraplingHook>().hooked)
+        {
+            gravity = 0;
+        }
+        else
+        {
+            gravity = -(2 * maxJumpHeight) / Mathf.Pow(timeToJumpApex, 2);
+        }
+
+        if (controller.collisions.left)
+        {
+            Debug.Log("Left");
+            var collider = this.GetComponent<BoxCollider>() as BoxCollider;
+            collider.center = new Vector3(-collider.center.x, collider.center.y, collider.center.z);
+        }
+        else if (controller.collisions.right)
+        {
+            Debug.Log("Right");
+            var collider = this.GetComponent<BoxCollider>() as BoxCollider;
+            collider.center = new Vector3(collider.center.x, collider.center.y, collider.center.z);
+        }
+
+        float targetVelocityX = directionalInput.x * (moveSpeed+4);
+        velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below) ? accelerationTimeGrounded : accelerationTimeAirborne);
+        velocity.y += gravity * Time.deltaTime;
+    }
+
+    public void RecieveDamageFromEnemy(float damage) //through this method object receives damage
+    {
+        currHP = currHP - damage;
+        if (currHP < 0) currHP = 0;
+        if (currHP > maxHP) currHP = maxHP;
+        //health_bar.fillAmount = currHP / maxHP;
+        if (currHP <= 0) Die();
+    }
+
+    protected  void Die() // things that happen while the object dies
+    {
+        Destroy(gameObject);
+    }
+
+    public void Damage()
+    {
+        //MeleeEnemy.SendMessage("RecieveDamage");
+        GameObject.FindGameObjectWithTag("Enemy").transform.SendMessage("RecieveDamageFromPlayer");
+    }
+
+}
---
+++ RunningRiot/Assets/Chunchunmaru's playground/Chun_Scriprs/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerPhysics))]
+public class PlayerController : MonoBehaviour {
+
+    public float gravity = 20;
+    public float speed = 8;
+    public float acceleration = 30;
+    public float jumpHeight = 12;
+
+    private float currSpeed;
+    private float targSpeed;
+    private Vector2 amountToMove;
+
+    private PlayerPhysics playerPhysics;
+	// Use this for initialization
+	void Start () {
+        playerPhysics = GetComponent<PlayerPhysics>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        targSpeed = Input.GetAxisRaw("Horizontal") * speed;
+        currSpeed = IncrementTowards(currSpeed, targSpeed, acceleration);
+
+        Debug.Log(playerPhysics.grounded);
+        if (playerPhysics.grounded)
+        {
+            amountToMove.y = 0;
+           
+            if (Input.GetButtonDown("Jump"))
+            {
+                amountToMove.y = jumpHeight;
+            }
+        }
+        else
+        {
+            amountToMove.y -= gravity * Time.deltaTime;
+        }
+        amountToMove.x = currSpeed;
+        
+        //amountToMove.y = 0;
+        playerPhysics.Move(amountToMove * Time.deltaTime);
+	}
+
+    private float IncrementTowards(float n, float target, float a)
+    {
+        if (n == target)
+        {
+            return n;
+        }
+        else
+        {
+            float dir = Mathf.Sign(target - n); // n must be increased/decreased to get closer to target
+            n += a * Time.deltaTime * dir;
+            return (dir == Mathf.Sign(target - n)) ? n : target; // if n passed target return n, otherwise - target
+        }
+    }
+}
